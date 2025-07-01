1e8ca3af0e7fffd5a8a19737165bf65be00d7a15
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-07-01 18:54:50 +0100

    data binding: simple data binding example

---
+++ src/main/java/com/znaji/walkthrough/databinding/UserForm.java
@@ -0,0 +1,44 @@
+package com.znaji.walkthrough.databinding;
+
+import java.time.LocalDate;
+
+public class UserForm {
+
+    private String name;
+    private String email;
+    private int age;
+    private LocalDate date;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
